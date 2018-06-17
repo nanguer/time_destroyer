e329c95e5cb8ff05fc753145120bc3c7914f36e0
nanguer <nahuelgimenez18@gmail.com>
2018-06-17 15:03:10 +0200

    add all files

---
+++ data.js
@@ -0,0 +1,184 @@
+var data = {
+	q: {
+		sound: new Howl({
+  		src: ['sounds/bubbles.mp3']
+		}),
+		color: '#1abc9c'
+		html5: true
+	},
+	w: {
+		sound: new Howl({
+  		urls: ['sounds/clay.mp3']
+		}),
+		color: '#2ecc71'
+		html5: true
+	},
+	e: {
+		sound: new Howl({
+  		urls: ['sounds/confetti.mp3']
+		}),
+		color: '#3498db'
+		html5: true
+	},
+	r: {
+		sound: new Howl({
+  		urls: ['sounds/corona.mp3']
+		}),
+		color: '#9b59b6'
+		html5: true
+	},
+		t: {
+		sound: new Howl({
+  		urls: ['sounds/dotted-spiral.mp3']
+		}),
+		color: '#34495e'
+		html5: true
+	},
+	y: {
+		sound: new Howl({
+  		urls: ['sounds/flash-1.mp3']
+		}),
+		color: '#16a085'
+		html5: true
+	},
+	u: {
+		sound: new Howl({
+  		urls: ['sounds/flash-2.mp3']
+		}),
+		color: '#27ae60'
+		html5: true
+	},
+	i: {
+		sound: new Howl({
+  		urls: ['sounds/flash-3.mp3']
+		}),
+		color: '#2980b9'
+		html5: true
+	},
+	o: {
+		sound: new Howl({
+			urls: ['sounds/glimmer.mp3']
+		}),
+		color: '#8e44ad'
+		html5: true
+	},
+	p: {
+		sound: new Howl({
+  		urls: ['sounds/moon.mp3']
+		}),
+		color: '#2c3e50'
+		html5: true
+	},
+	a: {
+		sound: new Howl({
+  		urls: ['sounds/pinwheel.mp3']
+		}),
+		color: '#f1c40f'
+		html5: true
+	},
+	s: {
+		sound: new Howl({
+  		urls: ['sounds/piston-1.mp3']
+		}),
+		color: '#e67e22'
+		html5: true
+	},
+		d: {
+		sound: new Howl({
+  		urls: ['sounds/piston-2.mp3']
+		}),
+		color: '#e74c3c'
+		html5: true
+	},
+	f: {
+		sound: new Howl({
+  		urls: ['sounds/prism-1.mp3']
+		}),
+		color: '#95a5a6'
+		html5: true
+	},
+	g: {
+		sound: new Howl({
+  		urls: ['sounds/prism-2.mp3']
+		}),
+		color: '#f39c12'
+		html5: true
+	},
+	h: {
+		sound: new Howl({
+  		urls: ['sounds/prism-3.mp3']
+		}),
+		color: '#d35400'
+		html5: true
+	},
+	j: {
+		sound: new Howl({
+  		urls: ['sounds/splits.mp3']
+		}),
+		color: '#1abc9c'
+		html5: true
+	},
+	k: {
+		sound: new Howl({
+  		urls: ['sounds/squiggle.mp3']
+		}),
+		color: '#2ecc71'
+		html5: true
+	},
+	l: {
+		sound: new Howl({
+  		urls: ['sounds/strike.mp3']
+		}),
+		color: '#3498db'
+		html5: true
+	},
+	z: {
+		sound: new Howl({
+  		urls: ['sounds/suspension.mp3']
+		}),
+		color: '#9b59b6'
+		html5: true
+	},
+	x: {
+		sound: new Howl({
+  		urls: ['sounds/timer.mp3']
+		}),
+		color: '#34495e'
+		html5: true
+	},
+	c: {
+		sound: new Howl({
+  		urls: ['sounds/ufo.mp3']
+		}),
+		color: '#16a085'
+		html5: true
+	},
+	v: {
+		sound: new Howl({
+  		urls: ['sounds/veil.mp3']
+		}),
+		color: '#27ae60'
+		html5: true
+	},
+	b: {
+		sound: new Howl({
+  		urls: ['sounds/wipe.mp3']
+		}),
+		color: '#2980b9'
+		html5: true
+	},
+	n: {
+		sound: new Howl({
+			src: ['sounds/zig-zag.mp3']
+		}),
+		color: '#8e44ad'
+		html5: true
+	},
+	m: {
+		sound: new Howl({
+  		src: ['sounds/moon.mp3']
+		}),
+		color: '#2c3e50'
+		html5: true
+	}
+}
